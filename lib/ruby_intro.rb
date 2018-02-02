ac9727a6d5fc9cf47a61343761339413137a3560
Arturo Perez <arturo@localhost.localdomain>
2018-02-02 17:50:52 -0600

    sum array, max2sum complete

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,14 +4,31 @@
 
 def sum arr
   # YOUR CODE HERE
+  numSum = 0
+  arr.each {|i| numSum+=i}
+  return numSum
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  #We can simply sort the array and iterate backwards
+  numSum = 0
+  if (arr.size == 0)
+    return numSum
+  end
+  if(arr.size == 1)
+    return arr.at(0)
+  end
+  maxFirst = arr.sort[-1];
+  maxSecond = arr.sort[-2];
+  numSum = maxFirst + maxSecond
+  return numSum
+
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  
 end
 
 # Part 2
